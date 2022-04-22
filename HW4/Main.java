e4ed0b1392c0ba9d40fc5a827e84437480143ae0
hwilt <henrywwilt@gmail.com>
2022-04-22 11:09:44 -0400

    HW5: Starting to work on it

---
+++ HW4/Main.java
@@ -0,0 +1,42 @@
+/**
+ * @author Henry Wilt
+ * Class: CS376, Hw3
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    /**
+     * ReadInConfig reads in the configuration file
+     * 
+     * @param configFile String location of the configuration file
+     * @return ArrayList<Int[]> of the configuration file
+     */
+    public static ArrayList<int[]> readInConfig(String configFile) {
+        ArrayList<int[]> ret = new ArrayList<int[]>();
+        try {
+            FileReader fr = new FileReader(configFile);
+            Scanner myReader = new Scanner(fr);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] dataArray = data.split(" ");
+                int[] dataInt = new int[dataArray.length];
+                for (int i = 0; i < dataArray.length; i++) {
+                    dataInt[i] = Integer.parseInt(dataArray[i]);
+                }
+                ret.add(dataInt);
+            }
+            myReader.close();
+        } catch (IOException e) {
+            System.out.println("File Error: " + e);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
